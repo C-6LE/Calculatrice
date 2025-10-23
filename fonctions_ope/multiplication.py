1837b8a69f89ccb1bfa15c18493b434d842b0832
Cécile <mme.guislain@gmail.com>
2025-10-23 12:26:14 +0200

    docs: formatage de la documentation

--- fonctions_ope/multiplication.py
+++ fonctions_ope/multiplication.py
@@ -1,9 +1,11 @@
 def multiplication(number_1:int|float, number_2:int|float) -> int|float :
     """
     Fonction multiplication qui permet de multiplier deux nombres et d'obtenir le resultat
+
     [Arguments]
     - number_1: int|float = premier nombre choisi par l'utilisateur
     - number_2: int|float = deuxième nombre choisi par l'utilisateur
+    
     [Return]
     - return number_1 multiplié par number_2
     """
