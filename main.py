7909eeceffe7735d6b63150f2bc08510efd6d800
Grumermer <grumermer12@gmail.com>
2025-10-23 16:55:40 +0200

    feature : changement de comportement de la calculatrice

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from fonctions_ope.division import division
 from fonctions_ope.puissance import puissance
 from fonctions_ope.modulo import modulo
 
-def demander_nombres() -> tuple[float, float]:
+def demander_nombres(text: str) -> float:
 
     """
-    Fonction qui demande a l'utilisateur de rentrer des nombres.
+    Fonction qui demande a l'utilisateur un nombre.
+
+    [Arguments]
+    - text: str = texte qui demande a l'utilisateur de rentrer un nombre
 
     [Return]
-    - return number_1 et number_2
+    - un float qui correspond a l'entrée de l'utilisateur
     """
 
     while True:
-
-        try: 
-            number_1 = float(input("Veuillez entrer votre premier nombre. \n"))
-            number_2 = float(input("Veuillez entrer votre deuxieme nombre. \n"))
-            return number_1, number_2
+        try:
+            return float(input(text))
         except ValueError:
             print("Entrée invalide")
             continue
@@ -37,6 +37,8 @@ def afficher_menu():
     print("6 - Modulo")
     print("7 - Quitter")
 
+resultat_precedent = None
+
 #Boucle qui permet de rejouer en boucle le calcul d'un type d'opération
 while True:
 
@@ -47,25 +49,36 @@ while True:
     except ValueError:
             print("Entrée invalide")
 
+    if resultat_precedent is not None:
+        print(f"\n resultat actuel : {resultat_precedent}")
+        utiliser = input(f"Souhaitez-vous utilisez {resultat_precedent} comme premier nombre ? (o / n)\n").lower()
+        if utiliser == "o":
+            number_1 = resultat_precedent
+        else:
+            number_1 = demander_nombres("Veuillez entrer votre premier nombre \n")
+    else:
+        number_1 = demander_nombres("Veuillez entrer votre premier nombre \n")
+    number_2 = demander_nombres("Veuillez entrer votre deuxieme nombre \n")
+
     match choice: 
         case 1:
-            number_1, number_2 = demander_nombres()
-            print(f"{number_1} + {number_2} = {addition(number_1 , number_2)}")
+            resultat_precedent = addition(number_1, number_2)
+            print(f"{number_1} + {number_2} = {resultat_precedent}")
         case 2:
-            number_1, number_2 = demander_nombres()
-            print(f"{number_1} - {number_2} = {soustraction(number_1 , number_2)}")
+            resultat_precedent = soustraction(number_1, number_2)
+            print(f"{number_1} - {number_2} = {resultat_precedent}")
         case 3:
-            number_1, number_2 = demander_nombres()
-            print(f"{number_1} x {number_2} = {multiplication(number_1 , number_2)}")
+            resultat_precedent = multiplication(number_1, number_2)
+            print(f"{number_1} x {number_2} = {resultat_precedent}")
         case 4:
-            number_1, number_2 = demander_nombres()
-            print(f"{number_1} / {number_2} = {division(number_1 , number_2)}")
+            resultat_precedent = division(number_1, number_2)
+            print(f"{number_1} / {number_2} = {resultat_precedent}")
         case 5:
-            number_1, number_2 = demander_nombres()
-            print(f"{number_1} ^ {number_2} = {puissance(number_1 , number_2)}")
+            resultat_precedent = puissance(number_1, number_2)
+            print(f"{number_1} ^ {number_2} = {resultat_precedent}")
         case 6:
-            number_1, number_2 = demander_nombres()
-            print(f"{number_1} % {number_2} = {modulo(number_1 , number_2)}")
+            resultat_precedent = modulo(number_1, number_2)
+            print(f"{number_1} % {number_2} = {resultat_precedent}")
         case 7:
             print("Fin de programme")
             break
