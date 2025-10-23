34f8dbdf5d59395d4202275a01eb633aecca0116
Cécile <mme.guislain@gmail.com>
2025-10-23 11:26:31 +0200

    refacto: changement nom variable + typehint + docstring fonction

--- fonctions_ope/addition.py
+++ fonctions_ope/addition.py
@@ -1,2 +1,11 @@
-def soustraction(a,b) :
-    return a+b
+def addition(number_1: int|float, number_2: int|float) :
+    """
+    Fonction addition qui permet d'additionner 2 nombres et d' obtenir le resultat
+    [Arguments]
+    - number_1: int|float = premier nombre choisi par l'utilisateur
+    - number_2: int|float = deuxième nombre choisi par l'utilisateur
+    [Return]
+    - return number_1 ajouté à number_2
+    """
+    return number_1 + number_2
+
